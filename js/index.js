dcdc8d2de95178b3e7957dc627b0b54116a9a4fe
nicolasnavarro94 <125922846+nicolasnavarro94@users.noreply.github.com>
2023-09-11 15:31:04 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ let recomprar = 0;
 let stop = 0;
 let entrada = 0;
 let tamano = 0;
+let gasto_total;
+let tamano_total;
 
 select.addEventListener("change", function () {
   const valorSeleccionado = select.value;
@@ -21,66 +23,82 @@ function start() {
   tipo = parseFloat(document.getElementById("tipo").value);
   recompra = parseFloat(document.getElementById("recompra").value);
   recomprar = parseFloat(document.getElementById("recomprar").value);
-  stop = parseFloat(document.getElementById("stop").value);
+  sl = parseFloat(document.getElementById("stop").value);
   entrada = parseFloat(document.getElementById("entrada").value);
   tamano = parseFloat(document.getElementById("tamano").value);
+  gasto_total = tamano * entrada;
+  tamano_total = tamano;
   if (
     !isNaN(tipo) &&
     !isNaN(recompra) &&
     !isNaN(recomprar) &&
-    !isNaN(stop) &&
+    !isNaN(sl) &&
     !isNaN(entrada) &&
     !isNaN(tamano)
   ) {
     calcular();
   } else {
-    // Al menos uno de los campos está vacío o no es un número válido
     alert("Por favor, complete todos los campos con números válidos.");
   }
 }
 function calcular() {
-  let precio_total = entrada * tamano;
-  let cantidad_total = tamano;
-  let promedio = entrada;
-  let seguir = true;
+  let continuar = true;
   let counter = 0;
-  let stop_loss = 0;
-  let porcentage = 0;
-
   const tbody = document.querySelector(".t-body");
   tbody.innerHTML = "";
-  while (seguir) {
-    if ((stop / precio_total) * 100 < recompra) {
-      seguir = false;
-      porcentage = (stop / precio_total) * 100;
-      if (tipo == 1) {
-        stop_loss = promedio - (promedio * porcentage) / 100;
-      } else {
-        stop_loss = promedio + (promedio * porcentage) / 100;
-      }
+  while (continuar) {
+    let nuevo_precio;
+    let stop_loss;
+    if (tipo == 1) {
+      nuevo_precio = entrada - (entrada * recompra) / 100;
+    } else {
+      nuevo_precio = entrada + (entrada * recompra) / 100;
+    }
+    let nueva_cantidad = tamano + (tamano * recomprar) / 100;
+    let nuevo_gasto_total = gasto_total + nueva_cantidad * nuevo_precio;
+    let promedio = nuevo_gasto_total / (nueva_cantidad + tamano_total);
+    let porcentage_stop = (sl / nuevo_gasto_total) * 100;
+
+    if (tipo == 1) {
+      stop_loss = promedio - (promedio * porcentage_stop) / 100;
+    } else {
+      stop_loss = promedio + (promedio * porcentage_stop) / 100;
+    }
+
+    if (porcentage_stop < recompra) {
+      console.log(
+        "stop: " +
+          stop_loss +
+          " cantidad: " +
+          tamano_total +
+          " usdt: " +
+          gasto_total
+      );
       const newRow = document.createElement("tr");
 
       newRow.innerHTML = `
 
                 <td class='table-danger text-danger'>Stop Loss</td>
                 <td class='table-danger'>${redondearNumero(stop_loss)}</td>
-                <td class='table-danger'>${redondearNumero(cantidad_total)}</td>
-                <td class='table-danger'>${redondearNumero(
-                  promedio * cantidad_total
-                )}</td>`;
+                <td class='table-danger'>${redondearNumero(tamano_total)}</td>
+                <td class='table-danger'>${redondearNumero(gasto_total)}</td>`;
 
       tbody.appendChild(newRow);
+      continuar = false;
     } else {
       counter++;
-      if (tipo == 1) {
-        entrada = entrada - (entrada * recompra) / 100;
-      } else {
-        entrada = entrada + (entrada * recompra) / 100;
-      }
-      tamano = tamano + (tamano * recomprar) / 100;
-      precio_total += entrada * tamano;
-      cantidad_total += tamano;
-      promedio = precio_total / cantidad_total;
+      entrada = nuevo_precio;
+      tamano = nueva_cantidad;
+      tamano_total += tamano;
+      gasto_total = nuevo_gasto_total;
+      console.log(
+        "Entrada: " +
+          entrada +
+          " Tamaño: " +
+          tamano +
+          " Usdt: " +
+          nueva_cantidad * nuevo_precio
+      );
       const newRow = document.createElement("tr");
       newRow.innerHTML = `
 
@@ -92,7 +110,7 @@ function calcular() {
                   tamano
                 )}</td>
                 <td class='rows bg-transparent text-white'>${redondearNumero(
-                  entrada * tamano
+                  nueva_cantidad * nuevo_precio
                 )}</td>`;
 
       tbody.appendChild(newRow);
@@ -102,7 +120,7 @@ function calcular() {
 
 function redondearNumero(numero) {
   if (numero < 1) {
-    return numero.toFixed(6); // Mantener números menores que 1 sin cambios
+    return numero.toFixed(6); 
   } else {
     return numero.toFixed(2);
   }
